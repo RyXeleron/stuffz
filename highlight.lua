a3f54c824795b6f7363e0b1039523a754efc69b5
RyXeleron <162963838+RyXeleron@users.noreply.github.com>
2024-06-30 21:15:15 +0700

    Create highlight.lua

---
+++ highlight.lua
@@ -0,0 +1,190 @@
+local Lexer = require(script.lexer)
+
+local defaultColors = require(script.Parent.Parent.Parent.Parent.Parent.Parent.Parent.Parent.config).codeColors;
+local TextService = game:GetService("TextService");
+
+local TokenColors = table.create(8);
+local TokenFormats = table.create(7)
+local ActiveLabels = table.create(3)
+
+local function SanitizeRichText(s)
+	return string.gsub(string.gsub(string.gsub(string.gsub(string.gsub(s,
+		"&", "&amp;"),
+		"<", "&lt;"),
+		">", "&gt;"),
+		"\"", "&quot;"),
+		"'", "&apos;"
+	)
+end
+
+local function SanitizeTabs(s)
+	return string.gsub(s, "\t", "    ")
+end
+
+local function SanitizeControl(s)
+	return string.gsub(s, "[\0\1\2\3\4\5\6\7\8\11\12\13\14\15\16\17\18\19\20\21\22\23\24\25\26\27\28\29\30\31]+", "")
+end
+
+local function highlight(textObject, src)
+	src = SanitizeTabs(SanitizeControl(src or textObject.Text))
+
+	textObject.RichText = false
+	textObject.Text = src
+	textObject.TextXAlignment = Enum.TextXAlignment.Left
+	textObject.TextYAlignment = Enum.TextYAlignment.Top
+	textObject.BackgroundColor3 = TokenColors.background
+	textObject.TextColor3 = TokenColors.iden
+	textObject.TextTransparency = 0.5
+
+	local textSize = textObject.TextSize
+
+	local _, numLines = string.gsub(src, "\n", "")
+	numLines += 1
+	
+	local size = TextService:GetTextSize("", textObject.TextSize, textObject.Font, Vector2.new());
+
+	local lineLabels = ActiveLabels[textObject]
+	if not lineLabels then
+		-- No existing lineLabels, create all new
+		lineLabels = table.create(numLines)
+		for i = 1, numLines do
+			local lineLabel = Instance.new("TextLabel")
+			lineLabel.Name = "Line_" .. i
+			lineLabel.RichText = true
+			lineLabel.BackgroundTransparency = 1
+			lineLabel.TextXAlignment = Enum.TextXAlignment.Left
+			lineLabel.TextYAlignment = Enum.TextYAlignment.Top
+			lineLabel.TextColor3 = TokenColors.iden
+			lineLabel.Font = textObject.Font
+			lineLabel.TextSize = textSize
+			
+			lineLabel.Size = UDim2.new(1, 0, 0, size.Y)
+			lineLabel.Position = UDim2.fromOffset(0, size.Y * textObject.LineHeight * (i - 1))
+			lineLabel.Text = ""
+
+			lineLabel.Parent = textObject
+			lineLabels[i] = lineLabel
+		end
+	elseif #lineLabels < numLines then
+		-- Existing labels, but missing some lines
+		for i = #lineLabels + 1, numLines do
+			local lineLabel = Instance.new("TextLabel")
+			lineLabel.Name = "Line_" .. i
+			lineLabel.RichText = true
+			lineLabel.BackgroundTransparency = 1
+			lineLabel.TextXAlignment = Enum.TextXAlignment.Left
+			lineLabel.TextYAlignment = Enum.TextYAlignment.Top
+			lineLabel.TextColor3 = TokenColors.iden
+			lineLabel.Font = textObject.Font
+			lineLabel.TextSize = textSize
+			
+			lineLabel.Size = UDim2.new(1, 0, 0, size.Y)
+			lineLabel.Position = UDim2.fromOffset(0, size.Y * textObject.LineHeight * (i - 1))
+			lineLabel.Text = ""
+
+			lineLabel.Parent = textObject
+			lineLabels[i] = lineLabel
+		end
+	elseif #lineLabels > numLines then
+		-- Existing labels, with too many lines
+		for i = #lineLabels, numLines, -1 do
+			lineLabels[i].Text = ""
+		end
+	end
+
+	local richText, index, lineNumber = {}, 0, 1
+	for token, content in Lexer.scan(src) do
+		local Color = TokenColors[token] or TokenColors.iden
+
+		local lines = string.split(SanitizeRichText(content), "\n")
+		for l, line in ipairs(lines) do
+			if l > 1 then
+				-- Set line
+				lineLabels[lineNumber].Text = table.concat(richText)
+				-- Move to next line
+				lineNumber += 1
+				index = 0
+				table.clear(richText)
+			end
+
+			index += 1
+			if Color ~= TokenColors.iden and string.find(line, "[%S%C]") then
+				richText[index] = string.format(TokenFormats[token], line)
+			else
+				richText[index] = line
+			end
+		end
+	end
+
+	-- Set final line
+	lineLabels[lineNumber].Text = table.concat(richText)
+
+	ActiveLabels[textObject] = lineLabels
+
+	local Cleanup
+	Cleanup = textObject.AncestryChanged:Connect(function()
+		if textObject.Parent then
+			return
+		end
+		ActiveLabels[textObject] = nil
+		Cleanup:Disconnect()
+	end)
+
+	return function()
+		for _, label in ipairs(lineLabels) do
+			label:Destroy()
+		end
+		table.clear(lineLabels)
+
+		ActiveLabels[textObject] = nil
+		Cleanup:Disconnect()
+	end
+end
+
+-- ty synapse
+--[[export type HighlighterColors = {
+	background: Color3?,
+	iden: Color3?,
+	keyword: Color3?,
+	builtin: Color3?,
+	string: Color3?,
+	number: Color3?,
+	comment: Color3?,
+	operator: Color3?
+}]]
+
+local function updateColors(colors)
+	-- Setup color data
+	TokenColors.background = (colors and colors.background) or Color3.fromRGB(47, 47, 47)
+	TokenColors.iden = (colors and colors.iden) or Color3.fromRGB(234, 234, 234)
+	TokenColors.keyword = (colors and colors.keyword) or Color3.fromRGB(215, 174, 255)
+	TokenColors.builtin = (colors and colors.builtin) or Color3.fromRGB(131, 206, 255)
+	TokenColors.string = (colors and colors.string) or Color3.fromRGB(196, 255, 193)
+	TokenColors.number = (colors and colors.number) or Color3.fromRGB(255, 125, 125)
+	TokenColors.comment = (colors and colors.comment) or Color3.fromRGB(140, 140, 155)
+	TokenColors.operator = (colors and colors.operator) or Color3.fromRGB(255, 239, 148)
+
+	for key, color in pairs(TokenColors) do
+		TokenFormats[key] = '<font color="#'
+			.. string.format("%.2x%.2x%.2x", color.R * 255, color.G * 255, color.B * 255)
+			.. '">%s</font>'
+	end
+
+	-- Rehighlight existing labels using latest colors
+	for label, lineLabels in pairs(ActiveLabels) do
+		for _, lineLabel in ipairs(lineLabels) do
+			lineLabel.TextColor3 = TokenColors.iden
+		end
+		highlight(label)
+	end
+end
+pcall(updateColors)
+
+return setmetatable({
+	UpdateColors = updateColors,
+	Highlight = highlight
+}, {
+	__call = function(_, textObject, src)
+		return highlight(textObject, src)
+	end
+})
