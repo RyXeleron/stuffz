0b5fdf88249df26e97fbffa81e5eb25effca20ec
RyXeleron <162963838+RyXeleron@users.noreply.github.com>
2024-06-30 21:16:22 +0700

    Create language.lua

---
+++ language.lua
@@ -0,0 +1,462 @@
+return {
+	keyword = {
+		["and"] = true,
+		["break"] = true,
+		["do"] = true,
+		["else"] = true,
+		["elseif"] = true,
+		["end"] = true,
+		["false"] = true,
+		["for"] = true,
+		["function"] = true,
+		["if"] = true,
+		["in"] = true,
+		["local"] = true,
+		["nil"] = true,
+		["not"] = true,
+		["while"] = true,
+		["or"] = true,
+		["repeat"] = true,
+		["return"] = true,
+		["then"] = true,
+		["true"] = true,
+		["self"] = true,
+		["until"] = true,
+		["continue"] = true,
+		["export"] = true,
+	},
+
+	builtin = {
+		-- Lua Functions
+		["assert"] = true,
+		["collectgarbage"] = true,
+		["error"] = true,
+		["getfenv"] = true,
+		["getmetatable"] = true,
+		["ipairs"] = true,
+		["loadstring"] = true,
+		["newproxy"] = true,
+		["next"] = true,
+		["pairs"] = true,
+		["pcall"] = true,
+		["print"] = true,
+		["rawequal"] = true,
+		["rawget"] = true,
+		["rawset"] = true,
+		["select"] = true,
+		["setfenv"] = true,
+		["setmetatable"] = true,
+		["tonumber"] = true,
+		["tostring"] = true,
+		["type"] = true,
+		["unpack"] = true,
+		["xpcall"] = true,
+
+		-- Lua Variables
+		["_G"] = true,
+		["_VERSION"] = true,
+
+		-- Lua Tables
+		["bit32"] = true,
+		["coroutine"] = true,
+		["debug"] = true,
+		["math"] = true,
+		["os"] = true,
+		["string"] = true,
+		["table"] = true,
+		["utf8"] = true,
+
+		-- Roblox Functions
+		["delay"] = true,
+		["elapsedTime"] = true,
+		["gcinfo"] = true,
+		["require"] = true,
+		["settings"] = true,
+		["spawn"] = true,
+		["tick"] = true,
+		["time"] = true,
+		["typeof"] = true,
+		["UserSettings"] = true,
+		["wait"] = true,
+		["warn"] = true,
+		["ypcall"] = true,
+
+		-- Roblox Variables
+		["Enum"] = true,
+		["game"] = true,
+		["shared"] = true,
+		["script"] = true,
+		["workspace"] = true,
+		["plugin"] = true,
+
+		-- Roblox Tables
+		["Axes"] = true,
+		["BrickColor"] = true,
+		["CatalogSearchParams"] = true,
+		["CellId"] = true,
+		["CFrame"] = true,
+		["Color3"] = true,
+		["ColorSequence"] = true,
+		["ColorSequenceKeypoint"] = true,
+		["DateTime"] = true,
+		["DockWidgetPluginGuiInfo"] = true,
+		["Faces"] = true,
+		["File"] = true,
+		["FloatCurveKey"] = true,
+		["Instance"] = true,
+		["NumberRange"] = true,
+		["NumberSequence"] = true,
+		["NumberSequenceKeypoint"] = true,
+		["OverlapParams"] = true,
+		["PathWaypoint"] = true,
+		["PhysicalProperties"] = true,
+		["PluginDrag"] = true,
+		["Random"] = true,
+		["Ray"] = true,
+		["RaycastParams"] = true,
+		["Rect"] = true,
+		["Region3"] = true,
+		["Region3int16"] = true,
+		["RotationCurveKey"] = true,
+		["task"] = true,
+		["TextChatMessageProperties"] = true,
+		["TweenInfo"] = true,
+		["UDim"] = true,
+		["UDim2"] = true,
+		["Vector2"] = true,
+		["Vector2int16"] = true,
+		["Vector3"] = true,
+		["Vector3int16"] = true,
+	},
+
+	libraries = {
+
+		-- Lua Libraries
+		math = {
+			abs = true,
+			acos = true,
+			asin = true,
+			atan = true,
+			atan2 = true,
+			ceil = true,
+			clamp = true,
+			cos = true,
+			cosh = true,
+			deg = true,
+			exp = true,
+			floor = true,
+			fmod = true,
+			frexp = true,
+			ldexp = true,
+			log = true,
+			log10 = true,
+			max = true,
+			min = true,
+			modf = true,
+			noise = true,
+			pow = true,
+			rad = true,
+			random = true,
+			round = true,
+			sinh = true,
+			sqrt = true,
+			tan = true,
+			tanh = true,
+			sign = true,
+			sin = true,
+			randomseed = true,
+
+			huge = true,
+			pi = true,
+		},
+
+		string = {
+			byte = true,
+			char = true,
+			find = true,
+			format = true,
+			gmatch = true,
+			gsub = true,
+			len = true,
+			lower = true,
+			match = true,
+			pack = true,
+			packsize = true,
+			rep = true,
+			reverse = true,
+			split = true,
+			sub = true,
+			unpack = true,
+			upper = true,
+		},
+
+		table = {
+			clear = true,
+			concat = true,
+			foreach = true,
+			foreachi = true,
+			freeze = true,
+			getn = true,
+			insert = true,
+			isfrozen = true,
+			maxn = true,
+			remove = true,
+			sort = true,
+			find = true,
+			pack = true,
+			unpack = true,
+			move = true,
+			create = true,
+		},
+
+		debug = {
+			dumpheap = true,
+			info = true,
+			profilebegin = true,
+			profileend = true,
+			resetmemorycategory = true,
+			setmemorycategory = true,
+			traceback = true,
+		},
+
+		os = {
+			time = true,
+			date = true,
+			difftime = true,
+			clock = true,
+		},
+
+		coroutine = {
+			create = true,
+			isyieldable = true,
+			resume = true,
+			running = true,
+			status = true,
+			wrap = true,
+			yield = true,
+		},
+
+		bit32 = {
+			arshift = true,
+			band = true,
+			bnot = true,
+			bor = true,
+			btest = true,
+			bxor = true,
+			countlz = true,
+			countrz = true,
+			extract = true,
+			lrotate = true,
+			lshift = true,
+			replace = true,
+			rrotate = true,
+			rshift = true,
+		},
+
+		utf8 = {
+			char = true,
+			codepoint = true,
+			codes = true,
+			graphemes = true,
+			len = true,
+			nfcnormalize = true,
+			nfdnormalize = true,
+			offset = true,
+
+			charpattern = true,
+		},
+
+		-- Roblox Libraries
+		Axes = {
+			new = true,
+		},
+
+		BrickColor = {
+			new = true,
+			New = true,
+			Random = true,
+			Black = true,
+			Blue = true,
+			DarkGray = true,
+			Gray = true,
+			Green = true,
+			Red = true,
+			White = true,
+			Yellow = true,
+			palette = true,
+			random = true,
+		},
+
+		CatalogSearchParams = {
+			new = true,
+		},
+
+		CellId = {
+			new = true,
+		},
+
+		CFrame = {
+			new = true,
+			Angles = true,
+			fromAxisAngle = true,
+			fromEulerAnglesXYZ = true,
+			fromEulerAnglesYXZ = true,
+			fromMatrix = true,
+			fromOrientation = true,
+			lookAt = true,
+			
+			identity = true,
+		},
+
+		Color3 = {
+			new = true,
+			fromRGB = true,
+			fromHSV = true,
+			fromHex = true,
+			toHSV = true,
+		},
+
+		ColorSequence = {
+			new = true,
+		},
+
+		ColorSequenceKeypoint = {
+			new = true,
+		},
+
+		DateTime = {
+			now = true,
+			fromIsoDate = true,
+			fromLocalTime = true,
+			fromUniversalTime = true,
+			fromUnixTimestamp = true,
+			fromUnixTimestampMillis = true,
+		},
+
+		DockWidgetPluginGuiInfo = {
+			new = true,
+		},
+
+		Faces = {
+			new = true,
+		},
+
+		FloatCurveKey = {
+			new = true,
+		},
+
+		Instance = {
+			new = true,
+		},
+
+		NumberRange = {
+			new = true,
+		},
+
+		NumberSequence = {
+			new = true,
+		},
+
+		NumberSequenceKeypoint = {
+			new = true,
+		},
+
+		OverlapParams = {
+			new = true,
+		},
+
+		PathWaypoint = {
+			new = true,
+		},
+
+		PhysicalProperties = {
+			new = true,
+		},
+
+		PluginDrag = {
+			new = true,
+		},
+
+		Random = {
+			new = true,
+		},
+
+		Ray = {
+			new = true,
+		},
+
+		RaycastParams = {
+			new = true,
+		},
+
+		Rect = {
+			new = true,
+		},
+
+		Region3 = {
+			new = true,
+		},
+
+		Region3int16 = {
+			new = true,
+		},
+
+		RotationCurveKey = {
+			new = true,
+		},
+
+		task = {
+			wait = true,
+			spawn = true,
+			delay = true,
+			defer = true,
+			synchronize = true,
+			desynchronize = true,
+		},
+
+		TweenInfo = {
+			new = true,
+		},
+
+		UDim = {
+			new = true,
+		},
+
+		UDim2 = {
+			new = true,
+			fromScale = true,
+			fromOffset = true,
+		},
+
+		Vector2 = {
+			new = true,
+			
+			one = true,
+			zero = true,
+			xAxis = true,
+			yAxis = true,
+		},
+
+		Vector2int16 = {
+			new = true,
+		},
+
+		Vector3 = {
+			new = true,
+			fromAxis = true,
+			fromNormalId = true,
+			FromAxis = true,
+			FromNormalId = true,
+			
+			one = true,
+			zero = true,
+			xAxis = true,
+			yAxis = true,
+			zAxis = true,
+		},
+
+		Vector3int16 = {
+			new = true,
+		},
+	},
+}
